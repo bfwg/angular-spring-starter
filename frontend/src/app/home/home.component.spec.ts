d3d99ad79f3fcd47be1fe601872d0638f959c26d
Vincent <ye.zheng@mail.utoronto.ca>
2017-06-22 16:56:05 -0400

    Unit test passed, e2e failed

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { ApiCardComponent, GithubComponent } from '../component';
+import {
+  MaterialModule
+} from '@angular/material';
+
+import {
+  ApiService,
+  AuthService,
+  UserService,
+  FooService,
+  ConfigService
+} from '../service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +20,23 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        HomeComponent,
+        ApiCardComponent,
+        GithubComponent
+      ],
+      imports: [
+        MaterialModule.forRoot(),
+      ],
+      providers: [
+        ApiService,
+        AuthService,
+        UserService,
+        FooService,
+        ConfigService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {

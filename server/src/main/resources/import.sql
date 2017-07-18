9d83c3106d7736c40a490c7a6bd03a5570d866f2
Fan Jin <fanjin1989@gmail.com>
2017-07-17 23:55:20 -0700

    refactor(server) remove dataed models (#16)
    - Remove DatedMoel adn DeletableModel as they are not realted to the
      purpose of this project. Having them will cause confustion to users.

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO user (id, username, password, firstname, lastname, created_at, updated_at, deleted_at) VALUES (1, 'user', '123', 'Fan', 'Jin', null, null, null);
-INSERT INTO user (id, username, password, firstname, lastname, created_at, updated_at, deleted_at) VALUES (2, 'admin', '123', 'Jing', 'Xiao', null, null, null);
+INSERT INTO user (id, username, password, firstname, lastname) VALUES (1, 'user', '123', 'Fan', 'Jin');
+INSERT INTO user (id, username, password, firstname, lastname) VALUES (2, 'admin', '123', 'Jing', 'Xiao');
 
-INSERT INTO authority (id, name, created_at, updated_at) VALUES (1, 'ROLE_USER', null, null);
-INSERT INTO authority (id, name, created_at, updated_at) VALUES (2, 'ROLE_ADMIN', null, null);
+INSERT INTO authority (id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO authority (id, name) VALUES (2, 'ROLE_ADMIN');
 
 INSERT INTO user_authority (user_id, authority_id) VALUES (1, 1);
 INSERT INTO user_authority (user_id, authority_id) VALUES (2, 1);

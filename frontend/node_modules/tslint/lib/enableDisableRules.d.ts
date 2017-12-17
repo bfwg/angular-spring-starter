3b47470431838785573bd46e7649c327dbb0665a
apeixinho <a_peixinho@sapo.pt>
2017-12-17 18:21:44 +0100

    Added /node_modules to gitignore. Removed node_modules directory

--- frontend/node_modules/tslint/lib/enableDisableRules.d.ts
+++
@@ -1,3 +0,0 @@
-import * as ts from "typescript";
-import { RuleFailure } from "./language/rule/rule";
-export declare function removeDisabledFailures(sourceFile: ts.SourceFile, failures: RuleFailure[]): RuleFailure[];

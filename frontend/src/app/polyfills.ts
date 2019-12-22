a630e6faca798c2d7b44606b5f41c0f1f79f91f1
Craig Stroberg <strobergcd@gmail.com>
2019-12-22 23:22:49 +0200

    feature/update

--- frontend/src/app/polyfills.ts
+++ frontend/src/app/polyfills.ts
@@ -41,8 +41,7 @@
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */

a337f6d52f5b9e5f9734e94b21509c27e14779cf
A. Peixinho <a_peixinho@sapo.pt>
2022-03-02 02:08:08 +0100

    Renamed server folder to backend. Updated dockerfiles

--- server/src/main/java/com/bfwg/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bfwg.repository;
-
-import com.bfwg.model.Authority;
-import com.bfwg.model.UserRoleName;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-    Authority findByName(UserRoleName name);
-}
